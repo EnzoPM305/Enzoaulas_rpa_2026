ec9cb45106eb72ab34482120812c3960412c57c3
Enzo Perez <enzopm305@gmail.com>
2026-08-26 13:40:04 -0300

    Resolução Desafio lab-01

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,18 @@
+Bot_Name: str =  "RPA_Financeiro"
+Max_Retries: int = 3
+Execution_Timeout: float = 30
+Is_Production: bool = False
+
+print("Inicializando o Processo!")
+print(50* "=")
+print("Bot_Name :", Bot_Name)
+print("Tipo :", type(Bot_Name))
+print("=" * 50)
+print("Max_Retries :", Max_Retries)
+print("Tipo :", type(Max_Retries))
+print("=" * 50)
+print("Execution_Timeout :", Execution_Timeout)
+print("Tipo :", type(Execution_Timeout))
+print(50 * "=")
+print("Is_Production :", Is_Production)
+print("Tipo :", type(Is_Production))
