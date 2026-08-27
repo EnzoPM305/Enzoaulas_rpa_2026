a7718608fafb17a482297d255dacba96a075cc3d
Enzo Perez <enzopm305@gmail.com>
2026-08-27 12:29:12 -0300

    Corrige erros de formatacao

--- AULA01/bot_initializer.py
+++ AULA01/bot_initializer.py
@@ -1,10 +1,10 @@
-Bot_Name: str =  "RPA_Financeiro"
+Bot_Name: str = "RPA_Financeiro"
 Max_Retries: int = 3
 Execution_Timeout: float = 30
 Is_Production: bool = False
 
 print("Inicializando o Processo!")
-print(50* "=")
+print(50 * "=")
 print("Bot_Name :", Bot_Name)
 print("Tipo :", type(Bot_Name))
 print("=" * 50)
@@ -16,3 +16,4 @@ print("Tipo :", type(Execution_Timeout))
 print(50 * "=")
 print("Is_Production :", Is_Production)
 print("Tipo :", type(Is_Production))
+print("Processo Inicializado com Sucesso!")
